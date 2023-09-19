04753ff7603daad47a0642372efd03fb32afc273
davidmhilton <dahilton@davidson.edu>
2023-09-19 15:52:41 -0400

    add js script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 
 /* javascript */
+console.log("This 'ad' employs a silly play on words using the brand Heinken."+ 
+"It seeks to motivate viewers to choose going to bed as an alternative to grabbing aother beer.")
 
 
